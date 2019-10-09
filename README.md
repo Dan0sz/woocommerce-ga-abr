6e1732bf2b0dba6b26260058075679f6bcc97c46
Dan0sz <info@daan.dev>
2019-10-09 21:11:58 +0200

    Version 1.1.0 / Edited README.md.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# woocommerce-ga-abr
-Adjusted Bounce Rate for WooCommerce Google Analytics Integration
+# Adjusted Bounce Rate for WooCommerce Google Analytics Integration
+
+This unofficial for WordPress adds Adjusted Bounce Rate to WooCommerce Google Analytics Integration.
+
+## Installation
+Click the big green button on this page to _clone or download_ the contents of this repository to `/path/to/wordpress-install-root/wp-content/plugins/woocommerce-ga-abr/`.
+
+Activate the plugin from within your WordPress Administrator-area in _Plugins > Installed Plugins_.
+
+## Configuration
+
+On line 22 of `woocommerce-ga-abr.php` you can adjust the Adjusted Bounce Rate's event timeout. Its default is 20 seconds.
---
+++ woocommerce-ga-abr.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @formatter:off
+ * Plugin Name: ABR for WC GA Integration
+ * Description: Adds an Adjusted Bounce Rate Event to WooCommerce Google Analytics Integration's tracking code.
+ * Version: 1.1.0
+ * Author: Daan van den Bergh
+ * Author URI: https://daan.dev
+ * License: GPL2v2 or later
+ * Text Domain: woocommerce-ga-abr
+ * @formatter:on
+ */
+
+/**
+ * This function adds the 'adjusted bounce rate' event $abr seconds after the pageview is sent.
+ */
+function daan_dev_add_adjusted_bounce_rate()
+{
+    /**
+     * Adjust $abr variable to modify the Adjusted Bounce Rate.
+     */
+    $abr = 30;
+
+    /************************************
+     * DO NOT MODIFY ANYTHING BELOW HERE!
+     ************************************/
+    $text         = "Pageview of $abr seconds or more";
+    $milliseconds = $abr * 1000;
+    $pageview     = "ga('send', 'pageview');\n";
+    $timeout      = "setTimeout(\"ga('send', 'event', 'adjusted bounce rate', '$text')\", $milliseconds);" . "\n";
+
+    wc_enqueue_js($pageview . $timeout);
+}
+
+/**
+ * There's a typo in the filter's handle.
+ * Might this plugin ever break in the future, it's probably because WooCommerce
+ * finally decided to fix this typo. :)
+ */
+add_filter('wc_goole_analytics_send_pageview', 'daan_dev_add_adjusted_bounce_rate');
